47094d6d021ff9ed570e2e0e5302fb2c030aadc7
Elizabeth Klenczar <eklenczar@gmail.com>
2023-04-26 10:55:39 -0400

    Changed desserts patch to include customer desserts

--- app/controllers/desserts_controller.rb
+++ app/controllers/desserts_controller.rb
@@ -1,18 +1,8 @@
 class DessertsController < ApplicationController
 
-    # get "/desserts" do
-    #     desserts = Dessert.all
-    #     desserts.to_json
-    # end
-
-    get "/desserts/available" do
+     get "/desserts/available" do
         desserts = Dessert.all.select {|pie| !pie.customer_id}
         desserts.to_json
-    end
-
-    get "/desserts/sold" do
-        dessert = Dessert.all.select {|pie| pie.customer_id}
-        dessert.to_json(include: :customer)
     end
 
     patch '/desserts/:id' do
@@ -20,7 +10,16 @@ class DessertsController < ApplicationController
         dessert.update(
           customer_id: params[:customer_id]
         )
-        dessert.to_json(include: :customer)
+        dessert.to_json(include: { customer: { include: :desserts } })
+    end
+
+    post '/desserts/available' do
+        dessert = Dessert.create(
+          flavor: params[:flavor],
+          description: params[:description],
+          price: params[:price],
+        )
+        dessert.to_json
     end
 
 end
